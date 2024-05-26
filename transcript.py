48f7f264c2203c144d64eacf05a10acd7589a64b
genesisdayrit <genesisdayrit@gmail.com>
2024-05-25 21:05:47 -0500

    update enter id text

--- transcript.py
+++ transcript.py
@@ -1,7 +1,7 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
 # Video ID of the YouTube video
-video_id = 'YVgH7XdGB2o'
+video_id = '<ENTER VIDEO ID HERE>'
 
 # Get the transcript
 transcript = YouTubeTranscriptApi.get_transcript(video_id)
